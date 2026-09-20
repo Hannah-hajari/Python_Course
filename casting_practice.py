f58d1233a3b5d9b02cfe571e602c9f4c278367f3
Hannah <hananeh.hajari98@gmail.com>
2026-09-20 21:53:56 +0200

    circle area casting

---
+++ casting_practice.py
@@ -0,0 +1,19 @@
+a=int(input("please enter a int number: "))
+print(float(a))
+
+
+b=float(input("please enter a float number: "))
+print(int(b))
+
+c=int(input("please enter a number: "))
+print("'",str(c),"'")
+
+d=input("please enter a string with number: ")
+print(int(d))
+
+
+v="15"
+print(int(v))
+
+g=int(input("please enter a string with number: "))
+print(bool(g))
